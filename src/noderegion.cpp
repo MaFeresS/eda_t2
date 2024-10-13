3c0ce442bce73d0daeebe094378a72d255f07421
MaFeres <maferes@miuandes.cl>
2024-10-12 22:00:13 -0300

    some changes

--- src/noderegion.cpp
+++ src/noderegion.cpp
@@ -1,10 +1,10 @@
 /* implementation of the class NodeRegion
  */
 #include <iostream>
-#include <stack>
+#include <vector>
 #include "image/point2D.hpp"
 
 namespace image{
-    template <class Point2D, class Container = deque<Point2D> > class stack;
+    vector<Point2D> stack
 }
 
